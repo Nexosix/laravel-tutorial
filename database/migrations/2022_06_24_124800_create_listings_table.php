3adb09f829b4d705d6f794dd00799a059817e055
Paweł Drozgowski <pawel_drozgowski@zut.edu.pl>
2022-07-01 11:26:02 +0200

    - add file upload and storing - add edit listing form and submit - add deleting listing

--- database/migrations/2022_06_24_124800_create_listings_table.php
+++ database/migrations/2022_06_24_124800_create_listings_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('logo')->nullable();
             $table->string('tags');
             $table->string('company');
             $table->string('location');
             $table->string('email');
             $table->string('website');
-            $table->string('description');
+            $table->longText('description');
             $table->timestamps();
         });
     }
@@ -31,8 +30,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('listings');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,6 +21,15 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 
 // Store listing data
 Route::post('/listings', [ListingController::class, 'store']);
+
+// Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
